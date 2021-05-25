93a77a65c771a8ed589fcd6dcd89c6b9383cceb1
taka-kon <camenaide@gmail.com>
2021-05-26 07:53:31 +0900

    first commit

---
+++ pages/blog/[id].jsx
@@ -0,0 +1,46 @@
+import styles from "../../styles/Home.module.scss"
+
+
+export default function BlogId({ blog }) {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>{blog.title}</h1>
+      <p className={styles.publishedAt}>{blog.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`,
+        }}
+        className={styles.post}
+      />
+    </main>
+  );
+}
+
+export const getStaticPaths = async () => {
+  const key = {
+    headers: {'X-API-KEY': process.env.API_KEY},
+  };
+  const data = await fetch('https://myblog1.microcms.io/api/v1/blog', key)
+    .then(res => res.json())
+    .catch(() => null);
+  const paths = data.contents.map(content => `/blog/${content.id}`);
+  return {paths, fallback: false};
+};
+
+export const getStaticProps = async context => {
+  const id = context.params.id;
+  const key = {
+    headers: {'X-API-KEY': process.env.API_KEY},
+  };
+  const data = await fetch(
+    'https://myblog1.microcms.io/api/v1/blog/' + id,
+    key,
+  )
+    .then(res => res.json())
+    .catch(() => null);
+  return {
+    props: {
+      blog: data,
+    },
+  };
+};
---
+++ pages/index.jsx
@@ -0,0 +1,34 @@
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home({ blog }) {
+  return (
+    <div>
+      <ul>
+        {blog.map(blog => (
+          <li key={blog.id}>
+            <Link href={`blog/${blog.id}`}>
+              <a>{blog.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async () => {
+  const key = {
+    headers: {'X-API-KEY': process.env.API_KEY},
+  };
+  const data = await fetch('https://myblog1.microcms.io/api/v1/blog', key)
+    .then(res => res.json())
+    .catch(() => null);
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
